3f118685825be1ab404e16cb1419219ee2a4e855
ZYYHsunshine <zzucgg@163.com>
2019-09-03 23:50:31 +0800

    day3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+import TodoList from './todoList'
+import store from './store'
 
-Vue.config.productionTip = false
-
+/* eslint-disable no-new */
 new Vue({
-    render: h => h(App),
-}).$mount('#app')
+  store,
+  el: '#app',
+  render: h => h(TodoList)
+})
